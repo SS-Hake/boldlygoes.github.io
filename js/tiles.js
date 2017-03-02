3bef54a5bc9ec138de1c8a741ca16c9201e7a42b
Sam Scott-Hake <sam_hake@live.co.uk>
2017-03-02 19:52:38 +0000

    Begin homepage changes - tiles and their css added.

---
+++ js/tiles.js
@@ -0,0 +1,13 @@
+﻿$(function(){
+     // See if this is a touch device
+     if ('ontouchstart' in window)
+     {
+        // Set the correct body class
+        $('.tile-wrap').removeClass('no-touch').addClass('touch');
+       
+        // Add the touch toggle to show text
+        $('div.boxInner img').click(function(){
+           $(this).closest('.boxInner').toggleClass('touchFocus');
+        });
+     }
+  });
